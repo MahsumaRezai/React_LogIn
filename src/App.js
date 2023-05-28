866f8926dd5b9a6e7334b304bf545938ca9fe805
Masoumeh Rezai <you@example.com>
2023-05-28 22:25:28 +0430

    work on useEffect section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -6,16 +6,20 @@ import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    localStorage.setItem("Hello to react", "1");
+    console.log("I love coding")
+
+  }, [isLoggedIn])
 
   const loginHandler = (email, password) => {
-    
+
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
     setIsLoggedIn(false);
   };
-
 
   return (
     <React.Fragment>
